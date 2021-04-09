298245b4c84727e32b08c888cb9b40c6331502c2
191240004 <191240004@smail.nju.edu.cn>
2021-04-09 20:50:21 +0800

    first

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,62 @@
 #include <stdio.h>
 #include <assert.h>
+#include <dirent.h>
+
+//select the number file 
+int filter(const struct dirent *dir){
+
+}
+
+int option(str argv){
+  if(argv=="-p" || argv=="--show-pids")
+      pstree_p();
+  else if (argv=="-V" || argv=="version")
+  {
+        /* code */
+  }
+  else if (argv=="n" || argv=="--numeric-sort")
+  {
+        /* code */
+  }
+}
+
+int get_pid()
+{
+
+}
+
+int pstree_n(){
+
+}
+
+int pstree_v(){
+
+}
+
+int pstree_p(){
+
+}
 
 int main(int argc, char *argv[]) {
+  int i,j,k;
+  char dir[1024],str[1024];
+  struct dirent **namelist;
+  int total=scandir("./proc",&namelist,filter,alphasort);//tiqu number file
+
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    if(argv[0]=="./pstree-32")
+    {
+     option(argv[1]);
+    }
+    else if (argv[0]=="./pstree-64")
+    {
+      /* code */
+    }
+    else
+      return printf("error");
   }
   assert(!argv[argc]);
-  return 0;
+
 }
